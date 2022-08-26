9b0510d258a07ab42f38346ec437d87ea1a1a07a
sKagaya <93765341+Ampdot@users.noreply.github.com>
2022-08-26 15:34:39 +0900

    ポーズ画面を仮で実装しました

--- OST_TeamA_Game/Assets/Scripts/ButtonResume.cs
+++ OST_TeamA_Game/Assets/Scripts/ButtonResume.cs
@@ -6,8 +6,22 @@ public class ButtonResume : MonoBehaviour
 {
     public void OnClick()
     {
+        closePauseMenu();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            closePauseMenu();
+        }
+    }
+
+    private void closePauseMenu()
+    {
         Time.timeScale = 1.0f;
         Destroy(transform.parent.gameObject);
+        Debug.Log("Pushed");
     }
 
 }
